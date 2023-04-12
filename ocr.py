5a716e6a2546dd5802c26ba7a059f21aebbb9d01
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-04-12 20:06:35 +0200

    feat: enable input of own PDF directory on command line

--- ocr.py
+++ ocr.py
@@ -5,7 +5,10 @@ from pdf2image import convert_from_path
 # pdf2image requires poppler on windows: https://github.com/Belval/pdf2image#windows
 import glob
 
-pdfs = glob.glob(r".\samplePDFs\*.pdf")
+target_path = input('Enter (full) path, which contains PDFs to convert or press enter to use ./samplePDFs by default:\n') or r".\samplePDFs"
+print(f'This is your path: {target_path}')
+
+pdfs = glob.glob(rf"{target_path}\*.pdf")
 
 for pdf_path in pdfs:
     pages = convert_from_path(pdf_path, 500, poppler_path = r".\poppler-23.01.0\Library\bin")
